19a2e2577f5c3fc7d931a8321e4c4479bebb96c4
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-23 11:17:50 +0330

    write the true variable into the javascript file

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,35 @@
+let meisam = "This is test for js file";
+
+// if (meisam === "This is test for js file") alert("Js is fun!");
+
+// console.log(10 + 15 + 80 - 5);
+
+let firstName = "Meisam";
+let lastName = "Mohammadi";
+
+let $function = 27;
+
+let name = "majid";
+
+let PI = 3.415;
+
+// It's better we use tihs funtion for wirtting stored variable
+
+let myFirstJob = "Programer";
+let mySecondJob = "Photograoher";
+
+// not this
+
+let jab1 = "Programer";
+let jab2 = "Photographer";
+
+console.log(
+  "My name is " +
+    firstName +
+    " " +
+    lastName +
+    " and my first job is " +
+    myFirstJob +
+    " and my second job is " +
+    mySecondJob
+);
