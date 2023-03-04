71c065c2a504d2c2ada44684125f8ddc0527faaa
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-03-04 14:12:44 +0330

    Coding Challenge #4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -521,16 +521,67 @@ GOOD LUCK 😀
 //   console.log("Not a Valid day");
 // }
 
-const age = 13;
+// const age = 13;
 
-const drink = age >= 18 ? "I like drink win 🍷" : "I like drink water 💧";
+// const drink = age >= 18 ? "I like drink win 🍷" : "I like drink water 💧";
 
-console.log(drink);
+// console.log(drink);
 
-let drink2;
-if (age >= 18) {
-  drink2 = "I like drink win 🍷";
-} else {
-  drink2 = "I like drink water 💧";
-}
-console.log(drink2);
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "I like drink win 🍷";
+// } else {
+//   drink2 = "I like drink water 💧";
+// }
+// console.log(drink2);
+
+/* 
+
+Coding Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+GOOD LUCK 😀
+
+*/
+
+// “The bill was 275, the tip was 41.25, and the total value
+// 316.25”
+
+// const bill = 430;
+// let tip;
+
+// if (bill >= 50 && bill <= 300) {
+//   tip = bill * (15 / 100);
+//   let total = bill + tip;
+//   console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+//     ${total}`);
+// } else if (bill >= 300) {
+//   tip = bill * (20 / 100);
+//   let total = bill + tip;
+//   console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+//     ${total}`);
+// }
+
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * (15 / 100) : bill * (20 / 100);
+// const tip1 = bill * (15 / 100);
+// const tip2 = bill * (20 / 100);
+const total = bill + tip;
+
+console.log(
+  `The bill was ${bill} , the tip was ${tip} and the total value ${total} `
+);
