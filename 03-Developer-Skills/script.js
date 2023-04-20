afbd868b0e5e199e792fcdf200257ac5920129fd
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-04-20 11:37:29 +0430

    write funtion Merge 2 array and fin the max and min and ignore the error

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,7 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-// PROBLEMS
+// PROBLEMS 1:
 // We work fir a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error"
 
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
@@ -22,14 +22,62 @@ const calcTemperatures = function (temps) {
   let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
     const curTemps = temps[i];
+    // For ignor erros
+    if (typeof curTemps !== "number") continue;
     // For max temps
     if (curTemps > max) max = curTemps;
     // For min temps
     if (curTemps < min) min = curTemps;
-    // For ignor erros
-    if (curTemps !== "number") continue;
   }
   console.log(max, min);
+  return max - min;
 };
 
-calcTemperatures(temperatures);
+const amplitud = calcTemperatures(temperatures);
+console.log(amplitud);
+
+// PROBLEMS 2:
+// 1) understanding the problems
+// - with 2 array, should we implement functinality twice? No, just merge two array
+// 2) Breaking up into sub-problems
+// - How to merge 2 array?
+
+const calcTemperaturesNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemps = temps[i];
+    // For ignor erros
+    if (typeof curTemps !== "number") continue;
+    // For max temps
+    if (curTemps > max) max = curTemps;
+    // For min temps
+    if (curTemps < min) min = curTemps;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudNew = calcTemperaturesNew([1, 2, 3, 4], [5, 6, 7, 8, 9]);
+console.log(amplitudNew);
+
+// const arrOne = [1, 2, 3, 4];
+// const arrTwo = [5, 6, 7, 8];
+
+// const mergArrays = function (t1, t2) {
+//   const arr1 = [0];
+//   const arr2 = [0];
+//   let merArr = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     merArr[i] = arr1[i];
+//   }
+//   for (let i = 0; i < arr2.length; i++) {
+//     merArr[arr1.length + i] = arr2[i];
+//   }
+//   console.log(merArr);
+// };
+
+// mergArrays(arrOne, arrTwo);
