5dc227e5235b9917a8765cf0c1a2a57667d3e137
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-05-06 03:39:27 +0430

    Guess Number - write functionality part low number and fixed correct and hiscore number

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -22,13 +22,24 @@ let score = 20;
     }
     if ( guess === secretNumber ) {
         document.querySelector('.message').textContent = '🎉 Corect number';
+        document.querySelector('.highscore').textContent = score;
+        document.body.style.backgroundColor = 'red'
     } else if ( guess > secretNumber) {
         if ( score > 1) {
-            document.querySelector('.message').textContent = '📈 Too High'
+            document.querySelector('.message').textContent = '📈 Too High!'
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = '🔥 You Lose!';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if ( score > 1) {
+            document.querySelector('.message').textContent = '📉 Too Low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '🔥 You Lose!'
             document.querySelector('.score').textContent = 0;
         }
     }
