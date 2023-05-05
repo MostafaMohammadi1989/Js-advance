2479be6a22aa77d8025155bb13d2865e9baaddad
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-05-06 03:23:26 +0430

    Guess Number - created a game with Dom and js and write parat about when you number is High

---
+++ 05-Guess-My-Number/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+// const h1 = 'meisam'
+// console.log(document.querySelector('.message').textContent = 'meisam mohammadi')
+
+document.querySelector('.message').textContent = '🎉 Corect number';
+document.querySelector('.number').textContent = 21;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+*/
+
+const secretNumber = document.querySelector('.number').textContent =  Math.trunc(Math.random()* 20 + 1)
+
+let score = 20;
+
+ document.querySelector('.check').addEventListener('click', function() {
+    const guess =Number(document.querySelector('.guess').value)
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!'
+    }
+    if ( guess === secretNumber ) {
+        document.querySelector('.message').textContent = '🎉 Corect number';
+    } else if ( guess > secretNumber) {
+        if ( score > 1) {
+            document.querySelector('.message').textContent = '📈 Too High'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '🔥 You Lose!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
