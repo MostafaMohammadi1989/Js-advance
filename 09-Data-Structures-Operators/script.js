28977f251a5c1c6cf77de41f408357c0b144328b
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-08-18 08:51:01 +0330

    Maps Iteration

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -47,6 +47,39 @@ const restaurant = {
 };
 
 
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🥇'],
+  [false, 'Try again']
+]);
+
+console.log('==>', question);
+//Convert object to map
+console.log('==>', Object.entries(openingHours));
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log('==>', hoursMap);
+
+
+//Quiz App
+console.log(question.get('question'))
+for (const [key, value] of question) {
+  if ( typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
+
+const answer = Number(prompt('Your Answer'))
+console.log('==>', answer);
+
+console.log('==>', question.get(question.get('correct') === answer));
+
+///////////////////////////////////////////////////
+
+// Map fundamental
+ /*
 const rest = new Map()
 
 rest.set('name', 'Clasicano Itaiano');
@@ -81,7 +114,8 @@ console.log('size==>', rest.size);
 
 rest.set(document.querySelector('h1'), 'Heading')
 console.log('==>', rest);
-
+*/
+//////////////////////////////////////////////////////////////////
 
 
 /*
